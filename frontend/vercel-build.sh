518eb707b9e1f3b6428728d8120c113dc71a38fe
rabita <tasmin.saira@gmail.com>
2025-01-24 19:59:01 +0600

    vercel updata3

--- frontend/vercel-build.sh
+++ frontend/vercel-build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-export NODE_OPTIONS="--openssl-legacy-provider"
-npm install
+
+# Install dependencies
+npm install --legacy-peer-deps
+
+# Set environment variables
+export NODE_OPTIONS=--openssl-legacy-provider
+export DISABLE_ESLINT_PLUGIN=true
+export CI=false
+export SKIP_PREFLIGHT_CHECK=true
+
+# Build the application
 npm run build 
