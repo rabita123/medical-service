d31693ffe47f975815a6d8bd81d5144ae62d4125
rabita <tasmin.saira@gmail.com>
2025-01-31 12:34:21 +0600

    deploy in netlify

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@ const config = {
   API_URL: window.env?.REACT_APP_API_URL || 'http://localhost:5001',
   NODE_ENV: window.env?.NODE_ENV || 'development',
   PUBLIC_URL: window.env?.PUBLIC_URL || '/',
-  PRODUCTION_API_URL: window.env?.REACT_APP_PRODUCTION_API_URL || 'https://medical-service-8f3zbu0r7-rabitas-projects.vercel.app'
+  PRODUCTION_API_URL: window.env?.REACT_APP_PRODUCTION_API_URL || 'https://medical-service-backend-eg8t.onrender.com'
 };
 
 export default config; 
